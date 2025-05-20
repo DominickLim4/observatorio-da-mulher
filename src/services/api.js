d6d16b0da2dafe0f6f5568f4481e78137c349d54
dominick <dominick.dominici@undb.edu.br>
2025-05-20 17:24:41 -0300

    mudanca na api

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
-const API_URL = 'https://mulheres-app-api.onrender.com/api';
+const API_URL = 'https://mulheres-app-api.onrender.com';
 
 const api = axios.create({ baseURL: API_URL });
 
 export const submitForm = async (formData) => {
- const response = await api.post('/forms', formData);
- return response.data;
+  const response = await api.post('/api/forms', formData);
+  return response.data;
 };
 
-/*export const getForms = async () => {
- const response = await api.get('/forms');
- return response.data;
-};*/
+export const getForms = async () => {
+  const response = await api.get('/api/forms');
+  return response.data;
+};
 
 export default api;
